e0cc8d8d41adc0645ec8778e940886a60d5665d7
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-10-21 10:02:52 +0200

    ejercicio1-3 terminado 4 no

---
+++ src/ejercicio3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println("introduce la base del triangulo");
+        Scanner in = new Scanner(System.in);
+        double base = in.nextDouble();
+        System.out.println("introduce la altura del traingulo");
+        double altura= in.nextDouble();
+        double perimetro= base*3;
+        System.out.println("la superficie del triangulo es " +perimetro);
+    }
+}
---
+++ src/ejercicio4.java
@@ -0,0 +1,7 @@
+
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+
+    }
+}
