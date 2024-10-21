393aa9cf548ed086c4ca1aded007cb6186c35966
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-10-21 09:31:37 +0200

    ejercicio1

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import static java.lang.System.in;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("introduce el lado del cuadrado");
