748fcaa3dda9188d9f7fb8c599ae03952b740618
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-10-21 09:29:44 +0200

    ejercicio1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("introduce el lado del cuadrado");
+        Scanner in  = new Scanner(System.in);
+        int lado= in.nextInt();
+        int superficie=lado*lado;
+        int perimetro=lado+lado+lado+lado;
+        System.out.println("la superficie es " +superficie);
+        System.out.println("el perimetro es "+ perimetro);
+
+
 
     }
 }
