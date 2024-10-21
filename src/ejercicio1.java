f34fc49d29867833e561ba7cdb2a04b75e2fd95e
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-10-21 09:36:22 +0200

    ejercicio1

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
+public class ejercicio1 {
     public static void main(String[] args) {
         System.out.println("introduce el lado del cuadrado");
-        Scanner in  = new Scanner(System.in);
-        int lado= in.nextInt();
-        int superficie=lado*lado;
-        int perimetro=lado+lado+lado+lado;
-        System.out.println("la superficie es " +superficie);
-        System.out.println("el perimetro es "+ perimetro);
-
-
+        Scanner in = new Scanner(System.in);
+        int lado = in.nextInt();
+        int superficie = lado * lado;
+        int perimetro = lado + lado + lado + lado;
+        System.out.println("la superficie es " + superficie);
+        System.out.println("el perimetro es " + perimetro);
 
     }
 }
