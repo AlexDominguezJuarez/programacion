c11fbaeb0ebc8f7fff1306ab97235b77f43e9bab
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-10-24 14:07:20 +0200

    1-18 terminados -15

---
+++ src/condicionales/ejercicio15.java
@@ -0,0 +1,25 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class ejercicio15 {
+    public static void main(String[] args) {
+        System.out.println("introduce un mes y el año");
+        Scanner in= new Scanner(System.in);
+        int year=in.nextInt();
+        String mes= in.next();
+        switch (mes == "enero" || "marzo" ||"mayo"|| "julio"|| "agosto" ||"octubre" ||"diciembre"):
+        case "enero":
+            System.out.println("el mes tiene 31 dias");
+        case "marzo":
+            System.out.println("el mes tiene 31 dias");
+        case "mayo":
+            System.out.println("el mes tiene 31 dias");
+        case "julio":
+            System.out.println("el mes tiene 31 dias");
+        case "agosto":
+            System.out.println("el mes tiene 31 dias");
+
+
+    }
+}
