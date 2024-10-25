2b8721cabdbae44799fe9f5a739ec9fd65483b2d
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-10-25 12:41:16 +0200

    1-2 terminados

---
+++ src/BUCLES/ejercicio1.java
@@ -0,0 +1,13 @@
+package BUCLES;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int numero_inicial=0;
+        while (numero_inicial<101){
+            System.out.println(numero_inicial);
+            numero_inicial=numero_inicial+2;
+
+        }
+
+    }
+}
---
+++ src/BUCLES/ejercicio2.java
@@ -0,0 +1,29 @@
+package BUCLES;
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("introduce 2 numeros");
+        Scanner in= new Scanner(System.in);
+        int numero1= in.nextInt();
+        int numero2=in.nextInt();
+        if (numero2>numero1) {
+            if (numero1 % 2 != 1) {
+                numero1=numero1+1;
+                while (numero1<numero2){
+                    System.out.println(numero1);
+                    numero1=numero1+2;
+
+                }
+            }else{
+                while (numero1<numero2){
+                    System.out.println(numero1);
+                    numero1=numero1+2;
+                }
+            }
+
+
+        }
+    }
+}
