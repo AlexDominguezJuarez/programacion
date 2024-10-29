7816f4fd6a426414af29767aa423c33e07d15e4c
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-10-29 13:12:20 +0100

    1-25terminados 26 no

--- src/BUCLES/ejercicio26.java
+++ src/BUCLES/ejercicio26.java
@@ -4,12 +4,29 @@ import java.util.Scanner;
 
 public class ejercicio26 {
     public static void main(String[] args) {
-        System.out.println("introduce un numero y te diremos si es par o no");
+        System.out.println("introduce un numero y te diremos si es primo o no");
 
-        Scanner in= new Scanner(System.in)
-        int numero=in.nextInt();
-        for (numero=numero;1;--){
+        Scanner in = new Scanner(System.in);
+        int numero1 = in.nextInt();
+        int numero2 = numero1;
+        numero1 = numero1 - 1;
+        int numero3 = 1;
+        for (numero1 = numero1; numero1 >= 2; numero1--) {
+            if (numero2 % numero1 == 0) {
+                numero3 = numero2 % numero1;
+                numero1 = 2;
 
-        }
+
+            }
+            }
+            if (numero3 == 0) {
+                System.out.println("el numero no es primo");
+
+            } else {
+                System.out.println("el numero  es primo");
+            }
+
+
+
     }
 }
---
+++ src/BUCLES/ejercicio27.java
@@ -0,0 +1,12 @@
+package BUCLES;
+
+public class ejercicio27 {
+    public static void main(String[] args) {
+        int numero1=2;
+        int numero2=numero1;
+         numero1=numero1-1;
+        int numero3=0;
+        int variable_numeros_primos=0;
+        for (numero1=numero1;variable_numeros_primos==20;numero1++);
+    }
+}
