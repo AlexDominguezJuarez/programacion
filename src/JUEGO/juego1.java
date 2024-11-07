480017acc4752929b816e20ef5d4f1e4a77d275c
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-11-07 13:56:30 +0100

    1-30

--- src/JUEGO/juego1.java
+++ src/JUEGO/juego1.java
@@ -2,14 +2,18 @@ package JUEGO;
 
 public class juego1 {
     public static void main(String[] args) {
-        int ataque1=85;
-        int defensa1=100;
-        int velocidad1=150;
-        int vida1= 90;
-        int ataque2=200;
-        int defensa2=90;
-        int velocidad3=110;
-        int vida2=190;
+        int ataque1=100;
+        int defensa1=120;
+        int velocidad1=160;
+        int vida1= 100;
+        int ataque2=175;
+        int defensa2=70;
+        int velocidad3=80;
+        int vida2=155;
+        int ronda=1;
+            while ( (vida1 | vida2) >=1 ){
+                System.out.println("ronda " +ronda);
+            }
 
 
     }
