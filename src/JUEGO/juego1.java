22124e5d7cee5191d8c919a3995de207657e60ac
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-11-07 13:27:08 +0100

    1-30

---
+++ src/JUEGO/juego1.java
@@ -0,0 +1,16 @@
+package JUEGO;
+
+public class juego1 {
+    public static void main(String[] args) {
+        int ataque1=85;
+        int defensa1=100;
+        int velocidad1=150;
+        int vida1= 90;
+        int ataque2=200;
+        int defensa2=90;
+        int velocidad3=110;
+        int vida2=190;
+
+
+    }
+}
