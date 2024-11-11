b0609e78016c0de02b02a101781f877a1e3157e5
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2024-11-11 10:03:21 +0100

    empezar con las rondas

--- src/JUEGO/juego1.java
+++ src/JUEGO/juego1.java
@@ -5,47 +5,84 @@ import java.util.Scanner;
 
 public class juego1 {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        int porcentaje = 10;
         System.out.println("Introduce los atributos del jugador 1, la suma de los atributos no puede sumar mas de 500");
-        System.out.println("Ataque jugador 1:");
-        int ataque1=in.nextInt();
-        System.out.println("Defensa jugador 1:");
-        int defensa1=in.nextInt();
-        int velocidad1=160;
-        int vida1= 100;
-        Random probabilidad_critico =new Random(20);
-        Random regeneracion1= new Random(30);
+        System.out.println("Ataque jugador 1: ");
+        int ataque1 = in.nextInt();
+        System.out.println("Defensa jugador 1: ");
+        int defensa1 = in.nextInt();
+        System.out.println("Velocidad jugador 1: ");
+        int velocidad1 = in.nextInt();
+        System.out.println("Vida jugador 1:");
+        int vida1 = in.nextInt();
+        Random pof = new Random(10);
+        int probabiliad_critico1 = pof.nextInt();
+        int regeneracion1 = vida1 / 10;
+        while (vida1 + ataque1 + defensa1 + velocidad1 > 500) {
+            System.out.println("valores demasiado superiores, introduzca de nuevo");
+            System.out.println("Ataque jugador 1: ");
+            ataque1 = in.nextInt();
+            System.out.println("Defensa jugador 1: ");
+            defensa1 = in.nextInt();
+            System.out.println("Velocidad jugador 1: ");
+            velocidad1 = in.nextInt();
+            System.out.println("Vida jugador 1:");
+            vida1 = in.nextInt();
+        }
 
-        int ataque2=175;
-        int defensa2=70;
-        int velocidad2=80;
-        int vida2=155;
-        Random probabilidad_critico2=new Random(12);
-        Random regeneracion2= new Random(20);
+        System.out.println("Introduce los atributos del jugador 2, la suma de los atributos no puede sumar mas de 500");
+        System.out.println("Ataque jugador 2: ");
+        int ataque2 = in.nextInt();
+        System.out.println("Defensa jugador 2: ");
+        int defensa2 = in.nextInt();
+        System.out.println("Velocidad jugador 2: ");
+        int velocidad2 = in.nextInt();
+        System.out.println("Vida jugador 2:");
+        int vida2 = in.nextInt();
+        int probabilidad_critico2 = pof.nextInt();
+        int regeneracion2 = vida2 / 10;
+        while (vida2 + ataque2 + defensa2 + velocidad2 > 500) {
+            System.out.println("Valores demasiado superiores, introduzca de nuevo");
+            System.out.println("Introduce los atributos del jugador 2, la suma de los atributos no puede sumar mas de 500");
+            System.out.println("Ataque jugador 2: ");
+            ataque2 = in.nextInt();
+            System.out.println("Defensa jugador 2: ");
+            defensa2 = in.nextInt();
+            System.out.println("Velocidad jugador 2: ");
+            velocidad2 = in.nextInt();
+            System.out.println("Vida jugador 2:");
+            vida2 = in.nextInt();
+        }
+        System.out.println(".........................................................................................................................................................................................................................");
+        System.out.println("ATRIBUTOS JUGADOR 1");
+        System.out.println("VIDA : " + vida1);
+        System.out.println("ATAQUE : " + ataque1);
+        System.out.println("DEFENSA : " + defensa1);
+        System.out.println("VELOCIDAD : " + velocidad1);
+        System.out.println("PROBABILIDAD DE CRÍTICO : " + porcentaje + "%");
+        System.out.println("REGENERACION : " + regeneracion1);
+        System.out.println(".........................................................................................................................................................................................................................");
+        System.out.println("ATRIBUTOS JUGADOR 2 ");
+        System.out.println("VIDA : " + vida2);
+        System.out.println("ATAQUE : " + ataque2);
+        System.out.println("DEFENSA : " + defensa2);
+        System.out.println("VELOCIDAD : " + velocidad2);
+        System.out.println("PROBABILIDAD DE CRÍTICO : " + porcentaje + "%");
+        System.out.println("REGENERACION : " + regeneracion2);
 
-        int accion=0;
+        int accion = 0;
 
-        int ronda=0;
-        while ( (vida1 | vida2) >=1 ){
-            ronda=ronda++;
-            System.out.println("ronda " +ronda);
-            if (velocidad1>velocidad2){
-                System.out.println("Juega primero el jugador 1 ");
-                System.out.println("¿Que accion quiere hacer el jugador 1?" +
-                        "1 :Ataque" +
-                        "2: Regeneracion");
-                accion= in.nextInt();
-                switch (accion){
-                    case 1:
-                        vida2=vida2+defensa2-ataque1;
+        int ronda = 0;
+        System.out.println(".........................................................................................................................................................................................................................");
+        while ((vida1|vida2) > 0){
 
-                }
+        }
 
 
 
-                }
-            }
 
 
     }
+
 }
