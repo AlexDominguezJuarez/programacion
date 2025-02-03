854b201c294ab6182dd76698b5c8868ff762f8c4
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2025-02-03 21:24:17 +0100

    no baja la vida ni na

--- src/TEMA3/HangmanGame.java
+++ src/TEMA3/HangmanGame.java
@@ -7,10 +7,15 @@ public class HangmanGame {
         System.out.println("Juego del ahorcado\nIntroduce una palabra\n");
         Scanner in=new Scanner(System.in);
         char letraIntroducida;
+        int eleccionDeCasilla=0;
         int intentos=6;
         int aciertos=0;
+        int numeroDeErrores=0;
         String palabraAAdivinar=in.nextLine();
         char []arrayPalabra=new char[palabraAAdivinar.length()];
+        for (int i=0;i<arrayPalabra.length;i++){
+            arrayPalabra[i]=palabraAAdivinar.charAt(i);
+        }
         char[]arrayOculta=new char[palabraAAdivinar.length()];
         for (int i=0;i<palabraAAdivinar.length();i++){
             arrayOculta[i]='_';
@@ -19,22 +24,58 @@ public class HangmanGame {
             arrayPalabra[i]=palabraAAdivinar.charAt(i);
         }
         System.out.println("Adivina la palabra");
-        while ((intentos>=0)&&(aciertos<arrayPalabra.length)) {
+        while ((numeroDeErrores<6)&&(aciertos<arrayPalabra.length)) {
+            System.out.println("Introduce en que posicion quires introducir la letra\n");
+            eleccionDeCasilla=in.nextInt()-1;
+            String basura=in.nextLine();
             System.out.println("Introduce una letra que creas que forma parte de la palabra,tienes " + intentos + " intentos");
             String palabraIntroducida=in.nextLine();
-            letraIntroducida=palabraIntroducida.charAt(0);
-            for (int i=0;i<arrayPalabra.length;i++){
-                if (arrayPalabra[i]==letraIntroducida){
-                    arrayOculta[i]=letraIntroducida;
-                    System.out.println("¡¡Letra correcta!!");
-                }else{
-                    System.out.println("Letra incorrecta");
 
-                }
-                for (int j=0;j<arrayOculta.length;j++){
-                    System.out.print (arrayOculta[j]);
-                }
+            letraIntroducida=palabraIntroducida.charAt(0);
+            if (arrayPalabra[eleccionDeCasilla]==letraIntroducida){
+                System.out.println("¡¡Felicidades,has acertado!!");
+                aciertos++;
+                arrayOculta[eleccionDeCasilla]=letraIntroducida;
+            }else {
+                System.out.println("Lo siento, has fallado");
+                numeroDeErrores++;
             }
+
+            for (int j=0;j<arrayOculta.length;j++){
+                System.out.print (arrayOculta[j]);
+            }
+            System.out.println();
+            System.out.println();
+            switch (numeroDeErrores) {
+                case 1:
+                    System.out.println(" o");
+                    break;
+                case 2:
+                    System.out.println("    o");
+                    System.out.println(" /");
+                    break;
+                case 3:
+                    System.out.println(" o");
+                    System.out.println(" / |");
+                    break;
+                case 4:
+
+                    System.out.println(" o");
+                    System.out.println(" / | \\");
+                    break;
+                case 5:
+                    System.out.println(" o");
+                    System.out.println(" / | \\");
+                    System.out.println(" /");
+                    break;
+                case 6:
+
+                    System.out.println(" o");
+                    System.out.println(" / | \\");
+                    System.out.println(" / \\");
+                    break;
+
+            }System.out.println();
 
         }
 
