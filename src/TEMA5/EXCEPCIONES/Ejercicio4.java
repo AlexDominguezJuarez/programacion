9f65e92bd82186bf2185f6dc69987c1819474958
Alex <alexdomijuarez@gmail.com>
2025-02-25 14:02:55 +0100

    fak

---
+++ src/TEMA5/EXCEPCIONES/Ejercicio4.java
@@ -0,0 +1,32 @@
+package TEMA5.EXCEPCIONES;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Random random=new Random();
+        Scanner in =new Scanner(System.in);
+        int dimensionArray=random.nextInt(100);
+        int [] array=new int[dimensionArray];
+        for (int i=0;i<array.length;i++){
+            int numeroDeArray=random.nextInt(10);
+            array[i]=numeroDeArray;
+        }
+        int numeroPosicion;
+        try {
+            System.out.println("¿Que posicion quieres mostrar?");
+            numeroPosicion=in.nextInt();
+            System.out.println("La posicion contiene: "+array[numeroPosicion]);
+
+
+        }catch (InputMismatchException exceptionI){
+            System.out.println("Valor incorrecto, introduzca un numero");
+        }catch (ExcepcionesEjerc){
+                System.out.println("Introduzca un numero del 1 al 10");
+            }
+        }
+
+    }
+
---
+++ src/TEMA5/EXCEPCIONES/ExcepcionesEjercicio4.java
@@ -0,0 +1,7 @@
+package TEMA5.EXCEPCIONES;
+
+public class ExcepcionesEjercicio4 extends Exception{
+    public ExcepcionesEjercicio4(){
+        System.out.println("Numero no valido, introduce un numero del 1 al 10");
+    }
+}
