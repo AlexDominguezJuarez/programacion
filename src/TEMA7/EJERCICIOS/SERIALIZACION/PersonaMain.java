204dcc045f901b18d95352a195b408ce05e0bea1
AlexDominguezJuarez <alexdomijuarez@gmail.com>
2025-04-08 19:25:49 +0200

    faltan los dos ultimos

--- src/TEMA7/EJERCICIOS/SERIALIZACION/PersonaMain.java
+++ src/TEMA7/EJERCICIOS/SERIALIZACION/PersonaMain.java
@@ -1,34 +1,58 @@
 package TEMA7.EJERCICIOS.SERIALIZACION;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PersonaMain {
     public static void main(String[] args) {
 
-        Persona nuevaPersona=new Persona("Manolo",22);
+        System.out.println("Pulse 1 si quiere añadir una persona al archivo persona");
+        System.out.println("Pulse 2 si quiere leer el archivo persona");
 
-        PersonaOutput escritura=new PersonaOutput();
+        Scanner in =new Scanner(System.in);
+        int eleccion=in.nextInt();
+        switch (eleccion){
+            case 1:
+                Persona nuevaPersona=new Persona("Manolo",22);
 
-        try {
-            escritura.abrir();
-            escritura.escribir(nuevaPersona);
-            escritura.cerrar();
+                PersonaOutput escritura=new PersonaOutput();
 
-        }catch(Exception e){
+                try {
+                    escritura.abrir();
+                    escritura.escribir(nuevaPersona);
+                    escritura.cerrar();
 
-        }
+                }catch(Exception e){
 
-        Persona persona;
-        PersonaInput entrada;
+                }
 
-        entrada = new PersonaInput();
-        try {
-            entrada.abrir();
-            do{
-                persona = entrada.leer();
+                Persona persona;
+                PersonaInput entrada;
 
-            }while ((persona!=null));
-            entrada.cerrar();
-        }catch (Exception e){
+                entrada = new PersonaInput();
+                try {
+                    entrada.abrir();
+                    do{
+                        persona = entrada.leer();
 
+                    }while ((persona!=null));
+                    entrada.cerrar();
+                }catch (Exception e) {
+            }
+                break;
+            case 2:
+                PersonaInput leer=new PersonaInput();
+                try {
+                    leer.abrir();
+                    Persona persona1=leer.leer();
+                    leer.cerrar();
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(persona1);
+                break;
         }
     }
 }
