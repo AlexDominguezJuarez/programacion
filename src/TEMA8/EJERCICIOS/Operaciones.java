b7d72b3e6b1a19b11d5f5a0f16cce688328684db
Alex <alexdomijuarez@gmail.com>
2025-05-12 11:50:59 +0200

    terminado

---
+++ src/TEMA8/EJERCICIOS/Operaciones.java
@@ -0,0 +1,91 @@
+package TEMA8.EJERCICIOS;
+
+import com.sun.source.doctree.SeeTree;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Operaciones {
+
+    Scanner in =new Scanner(System.in);
+
+    String sentenciaSQL="";
+
+
+    public void listadoAsignaturasBDD() throws SQLException {
+        Connection con2 = DriverManager.getConnection("jdbc:postgresql://magos.ctieiwmm6bkr.us-east-1.rds.amazonaws.com:5432/magos", "postgres", "12345678");
+
+        sentenciaSQL="SELECT nombre_asignatura FROM Asignaturas;";
+        PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL);
+
+        ResultSet resultados = sentencia.executeQuery();
+
+        while(resultados.next()){
+            String nombreAsignatura = resultados.getString("nombre_asignatura");
+            System.out.println("Asignatura: " + nombreAsignatura);
+        }
+
+    }
+
+    public void estudiantesCasa() throws SQLException {
+
+        String nombreCasa = in.nextLine();
+
+        Connection con2 = DriverManager.getConnection("jdbc:postgresql://magos.ctieiwmm6bkr.us-east-1.rds.amazonaws.com:5432/magos", "postgres", "12345678");
+
+        sentenciaSQL = "SELECT nombre,apellido FROM Estudiante INNER JOIN Casa ON Estudiante.id_casa=Casa.id_casa WHERE Casa.nombre_casa='" + nombreCasa + "';";
+        PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL);
+
+        ResultSet resultados = sentencia.executeQuery();
+
+        while (resultados.next()) {
+            String apellido = resultados.getString("apellido");
+            String nombre = resultados.getString("nombre");
+            System.out.println("Estudiante: " + nombre + " " + apellido);
+        }
+    }
+
+    public void mascotaEstudiante() throws SQLException {
+        String nombreEstudiante = in.nextLine();
+        in.nextLine();
+        String apellidoEstudiante = in.nextLine();
+
+
+        Connection con2 = DriverManager.getConnection("jdbc:postgresql://magos.ctieiwmm6bkr.us-east-1.rds.amazonaws.com:5432/magos", "postgres", "12345678");
+
+        sentenciaSQL = "SELECT nombre_mascota FROM Mascota INNER JOIN Estudiante ON Estudiante.id_estudiante=Mascota.id_estudiante WHERE Estudiante.nombre='"+nombreEstudiante+"' AND Estudiante.apellido='"+apellidoEstudiante+
+                "';";
+        PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL);
+
+        ResultSet resultados = sentencia.executeQuery();
+
+        while (resultados.next()) {
+                String nombre = resultados.getString("nombre_mascota");
+                System.out.println("Mascota: "+nombre);
+        }
+    }
+
+
+    public void numeroEstudiantesCasa() throws SQLException {
+        Connection con2 = DriverManager.getConnection("jdbc:postgresql://magos.ctieiwmm6bkr.us-east-1.rds.amazonaws.com:5432/magos", "postgres", "12345678");
+
+        sentenciaSQL="SELECT Casa.nombre AS nombre_casa, COUNT(Estudiante.id_estudiante) AS numero_estudiantes\n" +
+                "FROM Casa " +
+                "LEFT JOIN Estudiante ON Casa.id_casa = Estudiante.id_casa " +
+                "GROUP BY Casa.id_casa, Casa.nombre " +
+                "ORDER BY Casa.nombre;";
+
+        PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL);
+
+
+        ResultSet resultados = sentencia.executeQuery();
+
+        while(resultados.next()){
+            String numeroAlumnos = resultados.getString("numero_estudiantes");
+            String nombreCasa = resultados.getString("nombre_casa");
+            System.out.println("Nombre de la casa: " + nombreCasa + "   Numero de alumnos: "+numeroAlumnos);
+        }
+    }
+
+
+}
