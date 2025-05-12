811b6b347fa53ef9ced9850d3f990a64b841a1e4
Alex <alexdomijuarez@gmail.com>
2025-05-12 09:11:30 +0200

    terminado

--- src/TEMA7/EJERCICIOS/EJERCICIOVIDEOJUEGO/VideojuegoInput.java
+++ src/TEMA7/EJERCICIOS/EJERCICIOVIDEOJUEGO/VideojuegoInput.java
@@ -1,17 +1,84 @@
 package TEMA7.EJERCICIOS.EJERCICIOVIDEOJUEGO;
 
+
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class VideojuegoInput implements Serializable {
+
+
     String rutabinaria = "/home/aledomjua/IdeaProjects/programacion/src/TEMA7/FICHEROS_EJERCICIOS/ranking.dat";
 
-    try(ObjectInputStream ob = new ObjectInputStream(new FileInputStream(rutabinaria))
+    private FileInputStream file;
 
-    public VideojuegoInput() throws IOException {
-    })
+    private ObjectInputStream input;
 
 
+
+    //Abrir fichero
+
+    public void abrir() throws IOException {
+
+
+            file = new FileInputStream("C:\\Users\\Lenovo\\IdeaProjects\\programacion\\src\\TEMA7\\EJERCICIOS\\EJERCICIOVIDEOJUEGO\\ranking.ser");
+
+            input = new ObjectInputStream(file);
+
+        }
+
+
+
+
+
+    //Cerrar el fichero
+
+    public void cerrar() throws IOException{
+
+
+        if(input!=null){
+
+            input.close();
+    }
+
+    }
+
+
+    public Videojuego leer() throws IOException, ClassNotFoundException{
+
+
+        Videojuego videojuego = null;
+
+
+        if(input!=null){
+
+
+            try{
+
+
+                videojuego = (Videojuego) input.readObject();
+
+
+                System.out.println("contacto leido correctamente");
+
+
+
+
+            }catch (EOFException eof){
+
+
+                //Fin del fichero
+
+
+            }
+
+
+        }
+
+
+        return videojuego;
+
+
+    }
 
 }
